600060ed44f7e6c469f2464e30b31ce077bc76f0
David Huerta <huertanix@opentil.com>
2014-01-22 00:13:02 -0500

    raw untested camera script

--- raspi/plexicheese.rb
+++ raspi/plexicheese.rb
@@ -1,4 +1,18 @@
 require 'flickraw'
 
-flickr = FlickRaw::Flickr.new
+#flickr = FlickRaw::Flickr.new
+# ugh oauth
 
+loop do
+  raspi_user = "pi"
+  unixtime = Time.now.to_i
+
+  if system "raspistill -o /home/#{raspi_user}/plexius/#{unixtime}.jpg"
+    # Upload to ~THE CLOUD~
+
+  else
+    puts "ROFLCOPTER DOWN!"
+  end
+
+  sleep 60
+end
