fbd1c322a8e1d99825d2d5e06474f632ab352f96
David Huerta <huertanix@opentil.com>
2014-01-27 17:27:37 -0500

    pix directory is where pix go.

--- raspi/plexicheese.rb
+++ raspi/plexicheese.rb
@@ -2,7 +2,7 @@ loop do
   raspi_user = "pi"
   unixtime = Time.now.to_i
 
-  if system "raspistill -ISO 800 -rot 180 -o /home/#{raspi_user}/plexius/#{unixtime}.jpg"
+  if system "raspistill -ISO 800 -rot 180 -o /home/#{raspi_user}/pix/#{unixtime}.jpg"
     puts "#{unixtime}.jpg saved!"
   else
     puts "ROFLCOPTER DOWN!"
