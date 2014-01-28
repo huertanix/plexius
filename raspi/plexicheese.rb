d9baf1affeefc00eae82aef86023fb5cf141c02b
David Huerta <huertanix@opentil.com>
2014-01-28 10:35:45 -0500

    NO MOAR ROTATE!

--- raspi/plexicheese.rb
+++ raspi/plexicheese.rb
@@ -2,7 +2,7 @@ loop do
   raspi_user = "pi"
   unixtime = Time.now.to_i
 
-  if system "raspistill -ISO 800 -rot 90 -o /home/#{raspi_user}/pix/#{unixtime}.jpg"
+  if system "raspistill -ISO 800 -o /home/#{raspi_user}/pix/#{unixtime}.jpg"
     puts "#{unixtime}.jpg saved!"
   else
     puts "ROFLCOPTER DOWN!"
