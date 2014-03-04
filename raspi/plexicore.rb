314101a9193a7ebf7c20acee64c82896f1090c97
David Huerta <huertanix@opentil.com>
2014-03-03 23:15:34 -0500

    Literally copying and pasting a piper pi example.

--- raspi/plexicore.rb
+++ raspi/plexicore.rb
@@ -1,8 +1,11 @@
 require 'pi_piper'
 include PiPiper
 
-after :pin => 12, :goes => :high do
-  puts 'Button pressed!'
+#after :pin => 1, :goes => :high do
+#  puts 'Button pressed!'
+#end
+PiPiper.watch :pin => 17, :invert => true do |pin|
+  puts "Pin changed from #{pin.last_value} to #{pin.value}"
 end
 
 PiPiper.wait
