a46adcb8536c647bd07be84a197a0d881effc7e5
David Huerta <huertanix@opentil.com>
2014-03-03 22:59:22 -0500

    Switched to pip_piper gem.

--- raspi/plexicore.rb
+++ raspi/plexicore.rb
@@ -1,17 +1,8 @@
-require 'wiringpi'
+require 'pi_piper'
+include PiPiper
 
-gpio = WiringPi::GPIO.new
-# pin 1
-button = 1
-
-gpio.mode button, INPUT
-
-loop do
-  state = gpio.readAll
-  # manual override!
-  if state[button] == 0
-    system "ruby raspicheese.rb"
-  end
-
-  sleep 0.3
+after :pin => 1, :goes => :high do
+  puts 'Button pressed!'
 end
+
+PiPiper.wait
