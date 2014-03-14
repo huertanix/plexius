cb7a2014d7d4e98e38710d48cd474140c98f70b6
David Huerta <huertanix@opentil.com>
2014-03-13 21:35:11 -0400

    I <3 RSYNC! Solved #2.

--- raspi/plexiupload.sh
+++ raspi/plexiupload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo 'Uploading to server...'
-rsync --progress -ab --recursive ~/pix/ pi@93.95.227.123:/home/pi/pix/
+rsync --progress -ab --recursive --remove-source-files ~/pix/ pi@93.95.227.123:/home/pi/pix/
 echo 'Upload complete.'
