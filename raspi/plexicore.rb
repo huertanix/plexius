95a6f167df1d0c31c026d72d26bffaa02faf746b
David Huerta <huertanix@gmail.com>
2014-03-20 22:30:08 -0400

    omg shutter button totally works!

--- raspi/plexicore.rb
+++ raspi/plexicore.rb
@@ -1,11 +1,18 @@
 require 'pi_piper'
 include PiPiper
 
-#after :pin => 1, :goes => :high do
-#  puts 'Button pressed!'
-#end
-PiPiper.watch :pin => 17, :invert => true do |pin|
-  puts "Pin changed from #{pin.last_value} to #{pin.value}"
+# Based on: http://makezine.com/projects/tutorial-raspberry-pi-gpio-pins-and-python/
+
+#pin_shutter = PiPiper::Pin.new(:pin => 24, :direction => :in, :pull => :up)
+
+# It works, but maybe because the Python script reset something?
+after :pin => 24, :goes => :high do
+  puts 'Button pressed!'
 end
+
+# Also works
+#watch :pin => 24 do |pin|
+#  puts "Pin changed from #{pin.last_value} to #{pin.value}"
+#end
 
 PiPiper.wait
