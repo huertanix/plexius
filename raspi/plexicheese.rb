a995e8d09b2cd9d7a31a35e0d22b5353f2c440cf
guest <guest@guest-10.04>
2014-04-03 19:07:58 -0400

    Untested yolo swag 420 commit. Resolves #10.

--- raspi/plexicheese.rb
+++ raspi/plexicheese.rb
@@ -2,7 +2,7 @@ raspi_user = "pi"
 pix_directory = "pix"
 unixtime = Time.now.to_i
 
-if system "raspistill -ISO 800 -o /home/#{raspi_user}/#{pix_directory}/#{unixtime}.jpg"
+if system "raspistill -w 1024 -h 768 -ISO 800 -o /home/#{raspi_user}/#{pix_directory}/#{unixtime}.jpg"
   puts "#{unixtime}.jpg saved!"
 else
   puts "Failed to save #{unixtime}.jpg"
