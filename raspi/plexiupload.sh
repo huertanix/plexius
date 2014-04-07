bdaf6ae01811f2a19afe9de2ccc00faa4ca63a96
David Huerta <huertanix@gmail.com>
2014-04-06 22:28:17 -0400

    First attempt at utilizing ubergallery

--- raspi/plexiupload.sh
+++ raspi/plexiupload.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+server_ip="93.95.227.123"
+server_path="/var/www/plexius/web/gallery/"
 echo 'Uploading to server...'
-rsync --progress -ab --recursive --remove-source-files ~/pix/ pi@93.95.227.123:/home/pi/pix/
+rsync --progress -ab --recursive --remove-source-files ~/pix/ pi@$server_ip:$server_path
 echo 'Upload complete.'
